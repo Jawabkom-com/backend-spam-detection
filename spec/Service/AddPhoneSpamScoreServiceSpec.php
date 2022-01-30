6537d54370eeac6f07e6ea904a35cd9003ebb247
Mamdouh Zaqout <mamdouhzaq@gmail.com>
2022-01-30 18:32:10 +0200

    add test exception tests

--- spec/Service/AddPhoneSpamScoreServiceSpec.php
+++ spec/Service/AddPhoneSpamScoreServiceSpec.php
@@ -4,6 +4,9 @@ namespace spec\Jawabkom\Backend\Module\Spam\Detection\Service;
 
 use Jawabkom\Backend\Module\Spam\Detection\Contract\Entity\ISpamPhoneScoreEntity;
 use Jawabkom\Backend\Module\Spam\Detection\Contract\Repository\ISpamPhoneScoreRepository;
+use Jawabkom\Backend\Module\Spam\Detection\Exception\RequiredInputsException;
+use Jawabkom\Backend\Module\Spam\Detection\Exception\RequiredPhoneException;
+use Jawabkom\Backend\Module\Spam\Detection\Exception\RequiredSourceException;
 use Jawabkom\Backend\Module\Spam\Detection\Service\AddPhoneSpamScoreService;
 use Jawabkom\Backend\Module\Spam\Detection\Test\Classes\Entity\DummySpamPhoneScoreEntity;
 use Jawabkom\Backend\Module\Spam\Detection\Test\Classes\Repository\DummySpamPhoneScoreRepository;
@@ -47,4 +50,28 @@ class AddPhoneSpamScoreServiceSpec extends ObjectBehavior
         $result->shouldBeAnInstanceOf(ISpamPhoneScoreEntity::class);
     }
 
+    function it_should_throw_exception_if_data_not_provided()
+    {
+        $this->inputs([
+            'phone' => '',
+            'score' => 10,
+            'source' => 'test',
+            'country_code' => 'PS',
+            'tags' => []
+        ]);
+
+        $this->shouldThrow(RequiredPhoneException::class)->duringProcess();
+    }
+
+    public function it_should_throw_required_source_exception_if_not_source_provided()
+    {
+        $this->inputs([
+            'phone' => '+970599189357',
+            'score' => 10,
+            'country_code' => 'PS',
+            'tags' => []
+        ]);
+
+        $this->shouldThrow(RequiredSourceException::class)->duringProcess();
+    }
 }
--- src/Service/AddPhoneSpamScoreService.php
+++ src/Service/AddPhoneSpamScoreService.php
@@ -5,17 +5,22 @@ namespace Jawabkom\Backend\Module\Spam\Detection\Service;
 use Jawabkom\Backend\Module\Spam\Detection\Contract\Entity\ISpamPhoneScoreEntity;
 use Jawabkom\Backend\Module\Spam\Detection\Contract\Repository\ISpamPhoneScoreRepository;
 use Jawabkom\Backend\Module\Spam\Detection\Contract\Service\IAddPhoneSpamScoreService;
-use Jawabkom\Backend\Module\Spam\Detection\Exception\RequiredInputsException;
+use Jawabkom\Backend\Module\Spam\Detection\Exception\RequiredPhoneException;
+use Jawabkom\Backend\Module\Spam\Detection\Exception\RequiredSourceException;
 use Jawabkom\Standard\Abstract\AbstractService;
-use SleekDB\Store;
 
 class AddPhoneSpamScoreService extends AbstractService implements IAddPhoneSpamScoreService
 {
     public function process(): static
     {
         $phoneEntity = $this->di->make(ISpamPhoneScoreEntity::class);
+
+        if($this->getInput('phone') == null) throw new RequiredPhoneException();
         $phoneEntity->setPhone($this->getInput('phone'));
+
+        if($this->getInput('source') == null) throw new RequiredSourceException();
         $phoneEntity->setSource($this->getInput('source'));
+        
         $phoneEntity->setCountryCode($this->getInput('country_code'));
         $phoneEntity->setScore($this->getInput('score'));
         $phoneEntity->setTags($this->getInput('tags'));
