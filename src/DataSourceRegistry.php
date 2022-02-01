94da6dec303665fcc813975dd4e2c4a0646638f0
Mamdouh Zaqout <mamdouhzaq@gmail.com>
2022-02-01 15:03:03 +0200

    updates

---
+++ src/DataSourceRegistry.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jawabkom\Backend\Module\Spam\Detection;
+
+use Jawabkom\Backend\Module\Spam\Detection\Contract\DataSource\ISpamPhoneDataSource;
+use Jawabkom\Backend\Module\Spam\Detection\Contract\DataSource\ISpamPhoneDataSourceToEntityMapper;
+
+class DataSourceRegistry
+{
+    protected array $registry = [];
+
+    public function register(string $alias, ISpamPhoneDataSource $source, ISpamPhoneDataSourceToEntityMapper $mapper)
+    {
+        $this->registry[$alias]['source'] = $source;
+        $this->registry[$alias]['mapper'] = $mapper;
+    }
+
+    public function getRegistry(string $alias)
+    {
+        return $this->registry[$alias];
+    }
+
+    public function getSource(string $alias): ISpamPhoneDataSource
+    {
+        return $this->registry[$alias]['source'];
+    }
+
+    public function getMapper(string $alias): ISpamPhoneDataSourceToEntityMapper
+    {
+        return $this->registry[$alias]['mapper'];
+    }
+}
---
+++ src/Service/GetFromDataSourceListService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jawabkom\Backend\Module\Spam\Detection\Service;
+
+use Jawabkom\Backend\Module\Spam\Detection\Contract\Service\IAddUpdatePhoneSpamScoreService;
+use Jawabkom\Standard\Abstract\AbstractService;
+
+class GetFromDataSourceListService extends AbstractService implements IAddUpdatePhoneSpamScoreService
+{
+    
+    public function process(): static
+    {
+        // TODO: Implement process() method.
+    }
+}
